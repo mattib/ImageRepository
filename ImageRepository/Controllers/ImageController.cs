19cfb5913b0a650d8705b3dd70b483f9386a13dd
Matti Ben-Avraham <matti@Ben-Avraham.info>
2013-07-07 00:27:01 +0300

    First Commit

---
+++ ImageRepository/Controllers/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ImageRepository.Controllers
+{
+    public class ImageController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage Get(string fileId)
+        {
+             
+            //return new string[] { "value1", "value2" };
+
+            var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            Stream fs = File.OpenRead(@"H:\Matti's Document's\GitHub\ImageRepository\ImageRepository\Repository\ben-avraham.jpg");
+
+            httpResponseMessage.Content = new StreamContent(fs);
+
+            httpResponseMessage.Content.Headers.ContentType =
+                new MediaTypeHeaderValue("image/*");
+
+            httpResponseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("inline");
+            //httpResponseMessage.Content.Headers.ContentDisposition =
+            //    new ContentDispositionHeaderValue("attachment")
+            //    {
+            //        FileName = "ben-avraham.jpg"
+            //    };
+            return httpResponseMessage;
+        }
+
+    }
+}
---
+++ ImageRepository/Formaters/ImageTypeFormatter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ImageRepository.Formaters
+{
+    //public class ImageTypeFormatter : MediaTypeFormatter
+    //{
+    //    public ImageTypeFormatter()
+    //    {
+    //        SupportedMediaTypes.Clear();
+    //        SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/octet-stream"));
+    //    }
+
+    //    public override bool CanWriteType(Type type)
+    //    {
+    //        return true;// type == typeof(RepositoryFileAttributes);
+    //    }
+
+    //    public async override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content, System.Net.TransportContext transportContext)
+    //    {
+
+    //    }
+    //}
+}
